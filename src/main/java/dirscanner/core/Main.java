118fc4b12f501a63c982db209d0bda23325af7f1
M4tt1z1n <matt.sb@hotmailde>
2017-05-12 14:16:44 +0200

    Added missing args logging

--- src/main/java/dirscanner/core/Main.java
+++ src/main/java/dirscanner/core/Main.java
@@ -17,47 +17,46 @@ import dirscanner.logic.DirScanner;
 
 public class Main {
 
-    public static void main( final String[] args ) {
-       
-        Map<String, String> parsedArgs = parseArgs(args);
-        
-        DirScanner dirScanner = new DirScanner();
-        dirScanner.scan( new File(parsedArgs.get("-in")), parsedArgs.get("-fileType") );
-    }
-    
-    
-    private static Map<String, String> parseArgs(String[] args){
-    	
-    	Map<String, String> agrPairs = new HashMap<String, String>();
-    	if(args == null || args.length <= 0){
-    		//TODO ERROR
-    	} else {
-    		for (int i = 0; i < args.length; i = i +2){
-    			
-    			switch(args[i]){
-	    			case "-in":{
-	    				agrPairs.put(args[i], args[i+1]);
-	    				break;
-	    			}
-	    			case "-fileType":{
-	    				if((i+1) >= args.length){
-	    					agrPairs.put(args[i], "");
-	    				} else {
-	    					agrPairs.put(args[i], args[i+1]);
-	    				}	    				
-	    				break;
-	    			}
-	    			default: {
-	    				System.out.println("Unkown arg");
-	    				break;
-	    			}
-    			}
-    		}
-    	}
-    	
-    	if(agrPairs.isEmpty()){
-    		return null;    	
-    	}
+	public static void main(final String[] args) {
+
+		Map<String, String> parsedArgs = parseArgs(args);
+
+		DirScanner dirScanner = new DirScanner();
+		dirScanner.scan(new File(parsedArgs.get("-in")), parsedArgs.get("-fileType"));
+	}
+
+	private static Map<String, String> parseArgs(String[] args) {
+
+		Map<String, String> agrPairs = new HashMap<String, String>();
+		if (args == null || args.length <= 0) {
+			System.out.println("No args given please specify -in <directory>");
+		} else {
+			for (int i = 0; i < args.length; i = i + 2) {
+
+				switch (args[i]) {
+				case "-in": {
+					agrPairs.put(args[i], args[i + 1]);
+					break;
+				}
+				case "-fileType": {
+					if ((i + 1) >= args.length) {
+						agrPairs.put(args[i], "");
+					} else {
+						agrPairs.put(args[i], args[i + 1]);
+					}
+					break;
+				}
+				default: {
+					System.out.println("Unkown arg");
+					break;
+				}
+				}
+			}
+		}
+
+		if (agrPairs.isEmpty()) {
+			return null;
+		}
 		return agrPairs;
-    }
+	}
 }
